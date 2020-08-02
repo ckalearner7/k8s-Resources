d08730bebd0cfbb763c9c8ac291e87c1a8d6b41c
Sada Iyer <iyersada@gmail.com>
2020-08-01 19:16:25 -0600

    Create destroy-1.sh

---
+++ destroy-1.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# This script will modify the kube-apiserver file
+if [ -d ~/.save/ ]; then
+  echo "dir exists"
+else
+  echo "create dir"
+  mkdir ~/.save
+  echo "director created"
+fi
+suffix=`date +"%m-%d-%Y%T"`
+echo $suffix
+
+ORIG_FILENAME=kube-apiserver.yaml
+cp -p /etc/kubernetes/manifests/$ORIG_FILENAME ~/.save/$ORIG_FILENAME.$suffix
+sed 's/\/etc\/kubernetes\/pki\/ca.crt/\/etc\/kubernetes\/pki\/ca1.crt/' $ORIG_FILENAME > /tmp/a.yaml
+cp /tmp/a.yaml /etc/kubernetes/manifests/$ORIG_FILENAME

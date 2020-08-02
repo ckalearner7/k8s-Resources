ffb2821b2a4420031249c7f7a964f5341bc30aa8
Sada Iyer <iyersada@gmail.com>
2020-08-01 19:22:48 -0600

    Update destroy-1.sh

--- destroy-1.sh
+++ destroy-1.sh
@@ -5,12 +5,13 @@ if [ -d ~/.save/ ]; then
 else
   echo "create dir"
   mkdir ~/.save
-  echo "director created"
+  echo "directory created"
 fi
 suffix=`date +"%m-%d-%Y%T"`
 echo $suffix
 
 ORIG_FILENAME=kube-apiserver.yaml
 cp -p /etc/kubernetes/manifests/$ORIG_FILENAME ~/.save/$ORIG_FILENAME.$suffix
+cp -p /etc/kubernetes/manifests/$ORIG_FILENAME $ORIG_FILENAME
 sed 's/\/etc\/kubernetes\/pki\/ca.crt/\/etc\/kubernetes\/pki\/ca1.crt/' $ORIG_FILENAME > /tmp/a.yaml
 cp /tmp/a.yaml /etc/kubernetes/manifests/$ORIG_FILENAME
